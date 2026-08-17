ea5bb79aaf53ffd7b4c8cf58de23890cdf2c5267
Jemima <jemima.a.george@gmail.com>
2026-08-17 14:22:35 +0400

    Insert Records into Table

--- person.sql
+++ person.sql
@@ -6,3 +6,8 @@ CREATE TABLE person (
     date_of_birth DATE NOT NULL,
     email VARCHAR(150) 
 );
+
+INSERT INTO person (first_name, last_name, gender, date_of_birth) VALUES ('Anne', 'Mary', 'FEMALE', date '1980-10-31');
+INSERT INTO person (first_name, last_name, gender, date_of_birth, email) VALUES ('Damien', 'Lovegood', 'MALE', date '1982-10-31', 'damien@gmail.com');
+INSERT INTO person (first_name, last_name, gender, date_of_birth) VALUES ('Shayne', 'Top', 'MALE', date '1982-12-31');
+INSERT INTO person (first_name, last_name, gender, date_of_birth, email) VALUES ('Arasha', 'Heccox', 'FEMALE', date '1992-12-3', 'arasha@gmail.com');
